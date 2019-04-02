1ffab142f1ee90c7892d0fb28ee2a2f7fc678048
sgriffin53 <sgriffin53@gmail.com>
2019-04-02 01:01:27 +0100

    remove definition of SQUARE (redefined in chess.c)

--- Trappist/definitions.h
+++ Trappist/definitions.h
@@ -32,18 +32,6 @@ enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE
 // colors
 //enum { WHITE, BLACK, BOTH };
 
-// square numbers
-enum {
-	A1 = 21, B1, C1, D1, E1, F1, G1, H1,
-	A2 = 31, B2, C2, D2, E2, F2, G2, H2,
-	A3 = 41, B3, C3, D3, E3, F3, G3, H3,
-	A4 = 51, B4, C4, D4, E4, F4, G4, H4,
-	A5 = 61, B5, C5, D5, E5, F5, G5, H5,
-	A6 = 71, B6, C6, D6, E6, F6, G6, H6,
-	A7 = 81, B7, C7, D7, E7, F7, G7, H7,
-	A8 = 91, B8, C8, D8, E8, F8, G8, H8,
-	NO_SQ, OFFBOARD
-};
 
 // Castling permission constants
 enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
@@ -121,7 +109,7 @@ typedef struct {
 	int count;
 } PV;
 
-extern U64 pieceHash[13][120];
+extern U64 pieceHash[13][64];
 extern U64 turnHash;
-extern U64 castleHash[16];
+extern U64 castleHash[4];
 #endif
