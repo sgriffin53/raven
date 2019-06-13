da59e7077227d9ca0c28c71315f154bfaf6023e9
sgriffin53 <sgriffin53@gmail.com>
2019-06-13 18:02:46 +0100

    Changed reduction definition to use ply

--- search.h
+++ search.h
@@ -8,7 +8,7 @@
 void clearKillers(int ply);
 int alphaBeta(struct position *pos, int alpha, int beta, int depthleft, int nullmove, int ply, clock_t endtime);
 struct move search(struct position pos, int searchdepth,int movetime);
-int reduction(const struct move *move, const int depthleft, char cappiece, int legalmoves, int incheck);
+int reduction(const struct move *move, const int depthleft, char cappiece, int legalmoves, int incheck, int ply);
 int qSearch(struct position *pos, int alpha, int beta, int ply, clock_t endtime);
 
 #endif
