e32440376131eb81ca42eb607e37af2e3cd743cb
sgriffin53 <sgriffin53@gmail.com>
2021-05-02 19:54:35 +0100

    Removed bad assert.

--- src/chess/position.c
+++ src/chess/position.c
@@ -227,7 +227,6 @@ int getColour(struct position *pos, int sq) {
 	return NONE;
 }
 void setPiece(struct position *pos, int sq, int colour, int piece) {
-	assert(piece);
 	assert(pos);
 	assert(sq >= 0 && sq <= 63);
 	U64 BBsquare = (1ULL << sq);
